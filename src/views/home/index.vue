08a515099295d53081a517c95b4681cc606a95dd
xuejian <xuejian@xforceplus.com>
2019-04-18 18:01:06 +0800

    '轮播特效'

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,11 @@
 <template>
   <section id="home">
-    <div id="fn-home-carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2000">
+    <div
+      id="fn-home-carousel"
+      class="carousel slide carousel-fade"
+      data-ride="carousel"
+      data-interval="2000"
+    >
       <a class="item-prev" @click="goPrev" role="button" data-slide="prev">
         <i class="fa fa-angle-left"></i>
       </a>
@@ -8,10 +13,12 @@
         <i class="fa fa-angle-right"></i>
       </a>
       <div class="carousel-inner">
-        <div v-for="item in carousels" :key="item" :class="'item ' + item.active">
+        <div v-for="(item, index) in carousels" :key="index" :class="'item ' + item.active">
           <img :src="item.image" :alt="item.alt" class="img-responsive">
           <div class="carousel-caption">
-            <h1 class="animated fadeInDown delay-1"><span>{{item.title}}</span></h1>
+            <h1 class="animated fadeInDown delay-1">
+              <span>{{item.title}}</span>
+            </h1>
             <p class="animated fadeInDown delay-3">{{item.description}}</p>
             <a class="btn animated fadeInUp delay-4" :href="item.link">Read More</a>
           </div>
@@ -22,63 +29,82 @@
 </template>
 
 <script>
-    export default {
-      name: "Home",
-      data() {
-        return {
-          timer: "",
-          loopIndex: 0,
-          carousels: [
-            {
-              image: "/static/images/slider/slide-1.jpg",
-              alt: "第一张",
-              active: "active",
-              title: "awesome with winner",
-              description: "Build beautiful and professional sites in just moments ",
-              link: "#"
-            },
-            {
-              image: "/static/images/slider/slide-2.jpg",
-              alt: "第二张",
-              active: "",
-              title: "Fully Responsive",
-              description: "The perfect layout for your agency/portfolio site ",
-              link: "#"
-            },
-            {
-              image: "/static/images/slider/slide-3.jpg",
-              alt: "第三张",
-              active: "",
-              title: "Modern and elegant",
-              description: "One solution design with lot features",
-              link: "#"
-            }
-          ]
-        }
-      },
-      methods: {
-        autoLoop() {
-          this.goNext()
+export default {
+  name: 'Home',
+  data() {
+    return {
+      timer: '',
+      loopIndex: 0,
+      carousels: [
+        {
+          image: '/static/images/slider/slide-1.jpg',
+          alt: '第一张',
+          active: 'active',
+          title: 'awesome with winner',
+          description: 'Build beautiful and professional sites in just moments ',
+          link: "http://www.baidu.com"
         },
-        goPrev() {
-          this.carousels[this.loopIndex].active = ''
-          this.loopIndex = this.loopIndex == 0 ? (this.carousels.length - 1) : this.loopIndex - 1
-          this.carousels[this.loopIndex].active = 'active'
+        {
+          image: '/static/images/slider/slide-2.jpg',
+          alt: '第二张',
+          active: '',
+          title: 'Fully Responsive',
+          description: 'The perfect layout for your agency/portfolio site ',
+          link: '#'
         },
-        goNext() {
-          this.carousels[this.loopIndex].active = ''
-          this.loopIndex = this.loopIndex == (this.carousels.length - 1) ? 0 : this.loopIndex + 1
-          this.carousels[this.loopIndex].active = 'active'
+        {
+          image: '/static/images/slider/slide-3.jpg',
+          alt: '第三张',
+          active: '',
+          title: 'Modern and elegant',
+          description: 'One solution design with lot features',
+          link: '#'
         }
-      },
-      mounted() {
-        this.timer = setInterval(() => {
-          this.goNext
-        }, 5000)
-      }
+      ]
     }
+  },
+  methods: {
+    // 播放
+    play() {
+      this.timer = setInterval(this._next, 3000)
+    },
+    // 停止播放
+    stop() {
+      clearInterval(this.timer)
+    },
+    // 重启播放
+    restart() {
+      this.stop()
+      this.play()
+    },
+    // 往前翻一个
+    _prev() {
+      this.carousels[this.loopIndex].active = ''
+      this.loopIndex = this.loopIndex == 0 ? this.carousels.length - 1 : this.loopIndex - 1
+      this.carousels[this.loopIndex].active = 'active'
+    },
+    // 往后翻一个
+    _next() {
+      this.carousels[this.loopIndex].active = ''
+      this.loopIndex = this.loopIndex == this.carousels.length - 1 ? 0 : this.loopIndex + 1
+      this.carousels[this.loopIndex].active = 'active'
+    },
+    // 往前翻一个,并重启播放
+    goPrev() {
+      this._prev()
+      this.restart()
+    },
+    // 往后翻一个,并重启播放
+    goNext() {
+      this._next()
+      this.restart()
+    }
+  },
+  mounted() {
+    this.play()
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
