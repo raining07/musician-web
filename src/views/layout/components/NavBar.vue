39f9f9c55438f0129f3092d56ceaef4385a31167
xuejian <xuejian@xforceplus.com>
2019-04-19 17:37:34 +0800

    navbar适配

--- src/views/layout/components/NavBar.vue
+++ src/views/layout/components/NavBar.vue
@@ -6,14 +6,18 @@
       <div class="container">
         <!-- LOGO -->
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" @click="doCollapse"> 
+            <span class="icon-bar"></span> 
+            <span class="icon-bar"></span> 
+            <span class="icon-bar"></span> 
+          </button>
           <router-link class="navbar-brand" to="/"><img src="/static/images/logo.png" alt="logo"></router-link>
         </div>
         <!-- / END LOGO -->
-        <div class="navbar-collapse collapse">
+        <div :class="'navbar-collapse ' + this.collapse">
           <ul class="nav navbar-nav navbar-right">
-            <!-- 常规模块 -->
-            <li v-for="item in topRoutes">
+            <!-- 动态菜单 -->
+            <li v-for="(item, index) in topRoutes" :key="index" @click="doCollapse">
               <router-link :to="item.path"  class="smoothScroll">{{item.title}}</router-link>
             </li>
             <!-- 系统管理 -->
@@ -22,10 +26,10 @@
                 头像 <span class="caret"></span>
               </a>
               <ul role="menu" class="dropdown-menu">
-                <li>
+                <li @click="doCollapse">
                   <router-link to="#">密码修改</router-link>
                 </li>
-                <li>
+                <li @click="doCollapse">
                   <router-link to="#">退出</router-link>
                 </li>
               </ul>
@@ -46,7 +50,17 @@
       name: "NavBar",
       data () {
         return {
+          collapse: 'collapse',
           topRoutes : topRoutes.options.routes
+        }
+      },
+      methods: {
+        doCollapse() {
+          if (this.collapse == 'collapse') {
+            this.collapse = ''
+          } else if (this.collapse == '') {
+            this.collapse = 'collapse'
+          }
         }
       }
     }
